d7571f2c7015f9c44f4c2eb11de982561fa83573
BusterGreen <knucklescreations@gmail.com>
2024-06-03 17:31:37 -0400

    Initial commit

---
+++ server.lua
@@ -0,0 +1,14 @@
+local KGCore = exports['kg-core']:GetCoreObject()
+
+KGCore.Functions.CreateCallback('kg-spawn:server:getOwnedHouses', function(_, cb, cid)
+    if cid ~= nil then
+        local houses = MySQL.query.await('SELECT * FROM player_houses WHERE citizenid = ?', {cid})
+        if houses[1] ~= nil then
+            cb(houses)
+        else
+            cb({})
+        end
+    else
+        cb({})
+    end
+end)
